b927fff566ea85fbaf6bbc02eb97a47966ae9d8f
Romain Dartigues <romain.dartigues.ext@orange.com>
2024-05-17 14:48:59 +0200

    remove dead/unreachable code

--- uaa/utils_terraform.go
+++ uaa/utils_terraform.go
@@ -4,15 +4,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-// getListOfStructs
-//func getListOfStructs(v interface{}) []map[string]interface{} {
-//	vvv := []map[string]interface{}{}
-//	for _, vv := range v.([]interface{}) {
-//		vvv = append(vvv, vv.(map[string]interface{}))
-//	}
-//	return vvv
-//}
-
 // getChangedValueString
 func getChangedValueString(key string, updated *bool, d *schema.ResourceData) *string {
 
@@ -55,27 +46,6 @@ func getChangedValueBool(key string, updated *bool, d *schema.ResourceData) *boo
 	return nil
 }
 
-// getChangedValueIntList
-//func getChangedValueIntList(key string, updated *bool, d *schema.ResourceData) *[]int {
-//
-//	var a []interface{}
-//
-//	if d.HasChange(key) {
-//		a = d.Get(key).(*schema.Set).List()
-//		*updated = *updated || true
-//	} else if v, ok := d.GetOk(key); ok {
-//		a = v.(*schema.Set).List()
-//	}
-//	if a != nil {
-//		aa := []int{}
-//		for _, vv := range a {
-//			aa = append(aa, vv.(int))
-//		}
-//		return &aa
-//	}
-//	return nil
-//}
-
 // getChangedValueStringList
 func getChangedValueStringList(key string, updated *bool, d *schema.ResourceData) *[]string {
 	var a []interface{}
@@ -95,20 +65,6 @@ func getChangedValueStringList(key string, updated *bool, d *schema.ResourceData
 	}
 	return nil
 }
-
-// getChangedValueMap -
-//func getChangedValueMap(key string, updated *bool, d *schema.ResourceData) *map[string]interface{} {
-//
-//	if d.HasChange(key) {
-//		vv := d.Get(key).(map[string]interface{})
-//		*updated = *updated || true
-//		return &vv
-//	} else if v, ok := d.GetOk(key); ok {
-//		vv := v.(map[string]interface{})
-//		return &vv
-//	}
-//	return nil
-//}
 
 // getResourceChange -
 func getResourceChange(key string, d *schema.ResourceData) (bool, string, string) {
@@ -140,28 +96,3 @@ func getListChanges(old interface{}, new interface{}) (remove []string, add []st
 	}
 	return
 }
-
-// getListChangedSchemaLists -
-//func getListChangedSchemaLists(old []interface{}, new []interface{}) (remove []map[string]interface{}, add []map[string]interface{}) {
-//
-//	var a bool
-//
-//	for _, o := range old {
-//		remove = append(remove, o.(map[string]interface{}))
-//	}
-//	for _, n := range new {
-//		nn := n.(map[string]interface{})
-//		a = true
-//		for i, r := range remove {
-//			if reflect.DeepEqual(nn, r) {
-//				remove = append(remove[:i], remove[i+1:]...)
-//				a = false
-//				break
-//			}
-//		}
-//		if a {
-//			add = append(add, nn)
-//		}
-//	}
-//	return
-//}
